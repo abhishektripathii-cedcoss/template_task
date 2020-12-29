52fdec6ed643449d8ac393624c2b4bc30973ffcf
abhisheksaurabh7785 <abhisheksaurabh78663@gmail.com>
2020-12-29 20:22:10 +0530

    front-page and blogpost page dynamic

---
+++ wp-content/themes/xyz/header.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * The header for our theme
+ *
+ * This is the template that displays all of the <head> section and everything up until <div id="content">
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package xyz
+ */
+
+?>
+<!doctype html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="profile" href="https://gmpg.org/xfn/11">
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+<?php wp_body_open(); ?>
+<header class="">
+
+      <nav class="navbar navbar-expand-lg">
+        <div class="container">
+          <a class="navbar-brand" href="index.html"><h2>TASK-2<em>.</em></h2></a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarResponsive">
+
+	
+			<?php
+			wp_nav_menu(
+				array(
+					'theme_location' => 'menu-1',
+					'menu_id' => 'primary-menu',
+					'menu_class' => 'navbar-nav ml-auto',
+					'container'  => 'ul'
+					
+				)
+			);
+			?>
+			</div>
+		</div>
+		</nav><!-- #site-navigation -->
+	</header><!-- #masthead -->
