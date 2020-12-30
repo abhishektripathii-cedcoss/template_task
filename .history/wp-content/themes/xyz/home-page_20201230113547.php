41760dbca645a5e1ca6892bb6a268a4e48b8367e
abhisheksaurabh7785 <abhisheksaurabh78663@gmail.com>
2020-12-30 20:11:50 +0530

    pagination ,contact us page,blog post page display third commit

---
+++ .history/wp-content/themes/xyz/home-page_20201230113547.php
@@ -0,0 +1,383 @@
+
+                <?php
+/* Template Name: Homepage Template */
+
+
+
+get_header();
+?>
+
+<div class="main-banner header-text">
+      <div class="container-fluid">
+        <div class="owl-banner owl-carousel">
+            <!-- php code -->
+            <?php
+                  $args = array(
+                    'posts_per_page'   => 3,
+                    'post_type'        => 'post',
+                );
+                  $the_query = new WP_Query( $args );
+                  if ($the_query -> have_posts() ) { 
+                    while ( $the_query->have_posts() ){
+                          $the_query->the_post();
+                     
+            ?>
+          <div class="item">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/banner-item-01.jpg" alt="">
+            <div class="item-content">
+              <div class="main-content">
+                <div class="meta-category">
+                  <span>Fashion</span>
+                </div>
+                <a href="post-details.html"><h4>Morbi dapibus condimentum</h4></a>
+                <ul class="post-info">
+                  <li><a href="#">Admin</a></li>
+                  <li><a href="#">May 12, 2020</a></li>
+                  <li><a href="#">12 Comments</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <!-- <div class="item">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/banner-item-02.jpg" alt="">
+            <div class="item-content">
+              <div class="main-content">
+                <div class="meta-category">
+                  <span>Nature</span>
+                </div>
+                <a href="post-details.html"><h4>Donec porttitor augue at velit</h4></a>
+                <ul class="post-info">
+                  <li><a href="#">Admin</a></li>
+                  <li><a href="#">May 14, 2020</a></li>
+                  <li><a href="#">24 Comments</a></li>
+                </ul>
+              </div>
+            </div>
+          </div> -->
+          <!-- <div class="item">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/banner-item-03.jpg" alt="">
+            <div class="item-content">
+              <div class="main-content">
+                <div class="meta-category">
+                  <span>Lifestyle</span>
+                </div>
+                <a href="post-details.html"><h4>Best HTML Templates on TemplateMo</h4></a>
+                <ul class="post-info">
+                  <li><a href="#">Admin</a></li>
+                  <li><a href="#">May 16, 2020</a></li>
+                  <li><a href="#">36 Comments</a></li>
+                </ul>
+              </div>
+            </div>
+          </div> -->
+          <!-- <div class="item">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/banner-item-04.jpg" alt="">
+            <div class="item-content">
+              <div class="main-content">
+                <div class="meta-category">
+                  <span>Fashion</span>
+                </div>
+                <a href="post-details.html"><h4>Responsive and Mobile Ready Layouts</h4></a>
+                <ul class="post-info">
+                  <li><a href="#">Admin</a></li>
+                  <li><a href="#">May 18, 2020</a></li>
+                  <li><a href="#">48 Comments</a></li>
+                </ul>
+              </div>
+            </div>
+          </div> -->
+          <!-- <div class="item">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/banner-item-05.jpg" alt="">
+            <div class="item-content">
+              <div class="main-content">
+                <div class="meta-category">
+                  <span>Nature</span>
+                </div>
+                <a href="post-details.html"><h4>Cras congue sed augue id ullamcorper</h4></a>
+                <ul class="post-info">
+                  <li><a href="#">Admin</a></li>
+                  <li><a href="#">May 24, 2020</a></li>
+                  <li><a href="#">64 Comments</a></li>
+                </ul>
+              </div>
+            </div>
+          </div> -->
+          <!-- <div class="item">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/banner-item-06.jpg" alt="">
+            <div class="item-content">
+              <div class="main-content">
+                <div class="meta-category">
+                  <span>Lifestyle</span>
+                </div>
+                <a href="post-details.html"><h4>Suspendisse nec aliquet ligula</h4></a>
+                <ul class="post-info">
+                  <li><a href="#">Admin</a></li>
+                  <li><a href="#">May 26, 2020</a></li>
+                  <li><a href="#">72 Comments</a></li>
+                </ul>
+              </div>
+            </div>
+          </div> -->
+        </div>
+      </div>
+    </div>
+    <!-- Banner Ends Here -->
+
+    <section class="call-to-action">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12">
+            <div class="main-content">
+              <div class="row">
+                <div class="col-lg-8">
+                  <span>Stand Blog HTML5 Template</span>
+                  <h4>Creative HTML Template For Bloggers!</h4>
+                </div>
+                <div class="col-lg-4">
+                  <div class="main-button">
+                    <a rel="nofollow" href="<?php echo get_template_directory_uri(); ?>/https://templatemo.com/tm-551-stand-blog" target="_parent">Download Now!</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+
+    <section class="blog-posts">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-8">
+            <div class="all-blog-posts">
+              <!-- loop for post view -->
+                <?php
+                $args = array(
+                  'posts_per_page'   => 3,
+                  'post_type'        => 'post',
+              );
+              $the_query = new WP_Query( $args );
+                if ($the_query -> have_posts() ) : 
+                  while ( $the_query->have_posts() ):$the_query->the_post();
+                    //while ( $wpb_all_query->have_posts() ) : 
+                    // the_content();
+                    
+                    // the_excerpt() ;
+                     // echo "hello";
+                     echo '<br>';
+                
+              
+              
+                
+              
+              ?>
+
+
+
+             <!-- loop -->
+              <div class="row">
+                <div class="col-lg-12">
+                  <div class="blog-post">
+                    <div class="blog-thumb">
+                              <?php if ( has_post_thumbnail() ) { 
+                        the_post_thumbnail(get_the_ID(),'full');
+                        } ?>
+                    </div>
+                    <div class="down-content">
+                      <span class="category_custom_css_1"><?php the_category('|'); ?></span>
+                      <a href="post-details.html"><h4><?php the_title();?></h4></a>
+                      <ul class="post-info">
+                        
+                        <li><?php the_author();?></li>
+                        <li><?php the_modified_date()?></li>
+                        <li><?php comments_number();?></li>
+                      </ul>
+                      <?php the_excerpt()?>
+                      <div class="post-options">
+                        <div class="row">
+                          <div class="col-6">
+                            <ul class="post-tags">
+                              <li><i class="fa fa-tags"></i></li>
+                              <li><?php the_tags()?></li>
+                             
+                            </ul>
+                          </div>
+                          <div class="col-6">
+                            <ul class="post-share">
+                              <li><i class="fa fa-share-alt"></i></li>
+                              <li><a href="#">Facebook</a>,</li>
+                              <li><a href="#"> Twitter</a></li>
+                            </ul>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                     <!-- loop end -->
+                     <!-- loop for post view -->
+                        <?php
+                        
+                      endwhile;
+                   
+                      else :
+                          _e( 'Sorry, no posts matched your criteria.', 'textdomain' );
+                      endif;
+                        
+                        
+                        
+                        
+                        ?>
+                  </div>
+                </div>
+                
+
+
+
+               <!-- loop -->
+
+
+                <!-- loop end -->
+                <!-- <div class="col-lg-12">
+                  <div class="blog-post">
+                    <div class="blog-thumb">
+                      <img src="<?php //echo get_template_directory_uri(); ?>/assets/images/blog-post-02.jpg" alt="">
+                    </div>
+                    <div class="down-content">
+                      <span>Healthy</span>
+                      <a href="post-details.html"><h4>Etiam id diam vitae lorem dictum</h4></a>
+                      <ul class="post-info">
+                        <li><a href="#">Admin</a></li>
+                        <li><a href="#">May 24, 2020</a></li>
+                        <li><a href="#">36 Comments</a></li>
+                      </ul>
+                      <p>You can support us by contributing a little via PayPal. Please contact <a rel="nofollow" href="https://templatemo.com/contact" target="_parent">TemplateMo</a> via Live Chat or Email. If you have any question or feedback about this template, feel free to talk to us. Also, you may check other CSS templates such as <a rel="nofollow" href="https://templatemo.com/tag/multi-page" target="_parent">multi-page</a>, <a rel="nofollow" href="https://templatemo.com/tag/resume" target="_parent">resume</a>, <a rel="nofollow" href="https://templatemo.com/tag/video" target="_parent">video</a>, etc.</p>
+                      <div class="post-options">
+                        <div class="row">
+                          <div class="col-6">
+                            <ul class="post-tags">
+                              <li><i class="fa fa-tags"></i></li>
+                              <li><a href="#">Best Templates</a>,</li>
+                              <li><a href="#">TemplateMo</a></li>
+                            </ul>
+                          </div>
+                          <div class="col-6">
+                            <ul class="post-share">
+                              <li><i class="fa fa-share-alt"></i></li>
+                              <li><a href="#">Facebook</a>,</li>
+                              <li><a href="#">Twitter</a></li>
+                            </ul>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-lg-12">
+                  <div class="blog-post">
+                    <div class="blog-thumb">
+                      <img src="<?php //echo get_template_directory_uri(); ?>/assets/images/blog-post-03.jpg" alt="">
+                    </div>
+                    <div class="down-content">
+                      <span>Fashion</span>
+                      <a href="post-details.html"><h4>Donec tincidunt leo nec magna</h4></a>
+                      <ul class="post-info">
+                        <li><a href="#">Admin</a></li>
+                        <li><a href="#">May 14, 2020</a></li>
+                        <li><a href="#">48 Comments</a></li>
+                      </ul>
+                      <p>Nullam at quam ut lacus aliquam tempor vel sed ipsum. Donec pellentesque tincidunt imperdiet. Mauris sit amet justo vulputate, cursus massa congue, vestibulum odio. Aenean elit nunc, gravida in erat sit amet, feugiat viverra leo. Phasellus interdum, diam commodo egestas rhoncus, turpis nisi consectetur nibh, in vehicula eros orci vel neque.</p>
+                      <div class="post-options">
+                        <div class="row">
+                          <div class="col-6">
+                            <ul class="post-tags">
+                              <li><i class="fa fa-tags"></i></li>
+                              <li><a href="#">HTML CSS</a>,</li>
+                              <li><a href="#">Photoshop</a></li>
+                            </ul>
+                          </div>
+                          <div class="col-6">
+                            <ul class="post-share">
+                              <li><i class="fa fa-share-alt"></i></li>
+                              <li><a href="#">Facebook</a>,</li>
+                              <li><a href="#">Twitter</a></li>
+                            </ul>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div-->
+                <div class="col-lg-12">
+                  <div class="main-button">
+                    <a href="blog.html">View All Posts</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-4">
+            <div class="sidebar">
+              <div class="row">
+                <div class="col-lg-12">
+                  <div class="sidebar-item tags">
+                    <?php dynamic_sidebar('sidebar-1'); ?>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <!-- <div class="col-lg-4"> -->
+          
+            <!-- <div class="sidebar"> -->
+              <!-- <div class="row"> -->
+
+                <!-- <div class="col-lg-12"> -->
+              
+                  <!-- <div class="sidebar-item search"> -->
+
+                    <!-- <form id="search_form" name="gs" method="GET" action="">
+                      <input type="text" name="q" class="searchText" placeholder="type to search..." autocomplete="on">
+                    </form> -->
+                  <!-- </div> -->
+                <!-- </div> -->
+                <!-- <div class="col-lg-12"> -->
+                
+                  <!-- <div class="sidebar-item recent-posts"> -->
+              <?php //dynamic_sidebar( 'sidebar-1' );?>
+                 
+                    <!-- <div class="sidebar-heading"> -->
+                    
+                      <!-- <h2>Recent Posts</h2> -->
+                    <!-- </div> -->
+                    <!-- <div class="content"> -->
+                      <!-- <ul> -->
+                        <!-- <li><a href="post-details.html">
+                          <h5>Vestibulum id turpis porttitor sapien facilisis scelerisque</h5>
+                          <span>May 31, 2020</span>
+                        </a></li>
+                        <li><a href="post-details.html">
+                          <h5>Suspendisse et metus nec libero ultrices varius eget in risus</h5>
+                          <span>May 28, 2020</span>
+                        </a></li>
+                        <li><a href="post-details.html">
+                          <h5>Swag hella echo park leggings, shaman cornhole ethical coloring</h5>
+                          <span>May 14, 2020</span>
+                        </a></li> -->
+                      <!-- </ul> -->
+                    <!-- </div> -->
+                  <!-- </div> -->
+                <!-- </div> -->
+                
+                
+              <!-- </div> -->
+            <!-- </div> -->
+          </div>
+        </div>
+      </div>
+     
+    </section>
+
+<?php
+
+get_footer();
